2d424973b582f84d4fd53c356990500ba06b774a
Lindsey Jin <l44jin@uwaterloo.ca>
2019-01-19 13:18:55 -0500

    pushing modified file

--- api/functions/index.js
+++ api/functions/index.js
@@ -6,7 +6,7 @@
 //  response.send("Hello from Firebase!");
 // });
 
-const serviceAccount = require('./serviceAccountKey.json')
+// const serviceAccount = require('./serviceAccountKey.json')
 const express = require('express')
 const app = express();
 
@@ -16,22 +16,35 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 const dbName = 'describe-9e5c7';
-const bodyParser = require("body-parser")
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${dbName}.firebaseio.com`,
-  serviceAccount: serviceAccount
-});
+admin.initializeApp();
+
+app.use(express.json());
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.addMessage = functions.https.onRequest((req, res) => {
   // Grab the text parameter.
-  const original = req.body;
+  const original = JSON.parse(req.body);
+  // might need to parse this??? depending on what it looks like
+
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+  return admin.database().ref('/messages').push(original).then((snapshot) => {
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     return res.redirect(303, snapshot.ref.toString());
   });
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', function(req, res) {
+    const original = req.body;
+    res.send(original);
+    res.send("this works");
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
