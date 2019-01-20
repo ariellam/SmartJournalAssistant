e629b1ff901c13639ad1bc7b1c64457f97d9c9e2
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-20 04:50:42 -0500

    refactored data

--- client/src/data.js
+++ client/src/data.js
@@ -6,25 +6,53 @@ var data = [
     "flow": [
       {
         "prompt": "How was your day today?",
-        "response": "It was okay, I guess"
+        "response": "It was okay, I guess."
       },
       {
         "prompt": "Would you like to talk about it?",
-        "response": "Grade 12 student here. I'm president of model un, came fifth in deca icdc, and have a lot of side projects I can demonstrate. I know python and made 5k off of bitcoin too as well as ICOs back when they were popular. Now that I'm done with my ECs, what average do I need to get a gf? The waterloo application website has no info, so I'm assuming mid to high 90s? A lot of people in cs i spoke to seemed to have gotten deferred from gf though so is it only high 90s then? I was going to take the Euclid but I havent had time to study for it so I dont want my poor euclid scores to hurt my potential of getting gf. Idk if I should take it anyways. Also does gf have good co op opportunities? I'd like to try out a wide variety of positions and i dont know if gf would offer that. I heard laurier gf program can get you into a lot of places you wouldnt even think of, is this true? At the same time sometimes you get screwed over. Really I'm just looking for advice on how to get into this highly competitive program I hear everyone talking about."
+        "response": "Today I woke up at 6 a.m. I don’t like to wake up early but today I must go to school. I went to the bathroom and took a shower. Then I brushed my teeth. I put on my school uniform. I then went downstairs to eat breakfast. My mother and father were already in the kitchen. My father was reading the newspaper. My mother had cooked me fried eggs. I drank some orange juice while I watched the news on the television. I then got my school bag ready."
       }
     ]
   },
   {
-    "time_created": "January 5th, 2019",
-    "duration": "00:35:21",
+    "time_created": "January 12th, 2019",
+    "duration": "00:12:33",
     "flow": [
       {
         "prompt": "How was your day today?",
-        "response": "It was horrible."
+        "response": "Not so great."
       },
       {
         "prompt": "Would you like to talk about it?",
-        "response": "The room I was sitting in smelled like feet."
+        "response": "Today I woke up at 6 a.m. I don’t like to wake up early but today I must go to school. I went to the bathroom and took a shower. Then I brushed my teeth. I put on my school uniform. I then went downstairs to eat breakfast. My mother and father were already in the kitchen. My father was reading the Thai Rath newspaper. My mother had cooked me fried eggs. I drank some orange juice while I watched the news on the television. I then got my school bag ready."
+      }
+    ]
+  },
+  {
+    "time_created": "January 22nd, 2019",
+    "duration": "00:56:01",
+    "flow": [
+      {
+        "prompt": "How was your day today?",
+        "response": "It was nice."
+      },
+      {
+        "prompt": "Would you like to talk about it?",
+        "response": "Today our class teacher took us to a zoo. I was quite excited about the trip because I love animals. However, it turned out to be one of the saddest experiences of my life. We saw many birds and animals in the zoo. Alas! They didn’t look happy in their cages. There were colorful parrots and lovebirds. They looked sad in their small enclosures. I am sure they miss the blue skies. The plight of the lion, tiger and leopard was not much better either. They were kept inside tiny cages. They are well-fed and they also receive medical attention whenever they show signs of illness. Yet, they looked tired and unhealthy. I guess that is a result of their sedentary life. They get absolutely no opportunities to walk or run. They pace their small cages up and down looking restive and angry.I am not saying that zoos are bad. They do protect many endangered species. Still it hurts me to see wild animals caged for our entertainment. I know I can’t do anything for them but I can’t help feeling bad for them. I don’t think I will visit a zoo again."
+      }
+    ]
+  },
+  {
+    "time_created": "January 25th, 2019",
+    "duration": "00:10:28",
+    "flow": [
+      {
+        "prompt": "How was your day today?",
+        "response": "It was amazing!"
+      },
+      {
+        "prompt": "Would you like to talk about it?",
+        "response": "What an eventful day was today! I rescued a little girl from drowning in the river. I still can’t believe that I did it. How could I do this? Where did it I get the courage from? I don’t know. I was going to the temple when I heard a child yelling for help from the riverbank. I rushed there and saw a boy shouting for help. His sister had fallen into the river and he couldn’t swim. Neither could she. I saw her flapping with her arms. I also cried for help hoping that someone would come but when I saw nobody anywhere near the river I gathered the courage and jumped into the water. By then, the girl was beginning to sink. I swam with all my strength and managed to get hold of her hair. While it is true that I can swim holding her head above water while swimming back to the shore was quite a challenge for me. Luckily, by the time we reached the shore people had gathered and someone gave her emergency medical aid and rushed her to hospital. Everyone praised me for saving the life of a child putting my own life at risk. I felt both happy and proud."
       }
     ]
   }   
