44c3fcf346da12adfb6bdf469abbd7b143aa1e5f
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-20 00:47:59 -0500

    basic component setup

---
+++ client/src/components/JournalPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+class JournalPage extends Component {  
+  render() {
+    return (
+      <div className="JournalPage">
+        <h2>Journal Page</h2>
+      </div>
+    );
+  }
+}
+
+export default JournalPage;
