9d8176b24180441e949957abdea5c66897971b25
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-20 04:32:52 -0500

    journal entries now render real data

--- client/src/components/JournalPage.jsx
+++ client/src/components/JournalPage.jsx
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import JournalPreview from './JournalPreview';
+import JournalText from './JournalText';
+import {
+  getConvoData
+} from '../actions/actions.js'
 
 class JournalPage extends Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      // convoData: this.props.getConvoData() 
+    };
+    
+  }
+
   render() {
+    // console.log(this.props.convoData[0]);
     return (
       <div className="JournalPage">
-        <div class="row">
-          <div class="col-4">
-            <div class="list-group" id="list-tab" role="tablist">
-              <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">
-                <div class="d-flex w-100 justify-content-between">
-                  <h5 class="mb-1">List group item heading</h5>
-                  <small>3 days ago</small>
-                </div>
-                <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              </a>
-              <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-              <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-              <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
-            </div>
+        <div className="row">
+          <div className="col-4">
+            <JournalPreview/>
           </div>
-          <div class="col-8">
-            <div class="tab-content" id="nav-tabContent">
-              <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">asdf</div>
-              <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-              <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-              <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-            </div>
+          <div className="col-8">
+            <JournalText/>
           </div>
         </div>        
       </div>
     );
   }
 }
+
+// const mapStateToProps = state => {
+//   return {
+//     convoData: state.convoData
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     getConvoData: () => dispatch(getConvoData())
+//   };
+// }
+
+// export default connect(
+//   mapStateToProps, mapDispatchToProps
+// )(JournalPage);
 
 export default JournalPage;
