4c960d13ea026a5f5cc929f6a545fbac92bf6650
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2019-01-20 02:17:11 -0500

    added clickable tabs to journal page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import TitlePage from './components/TitlePage';
+import JournalPage from './components/JournalPage';
+import DashboardPage from './components/DashboardPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TitlePage/>
+        <JournalPage/>
       </div>
     );
   }
